608aa23d49e8af2563f4e9a2746c9e6aaf85871c
Sandy <sandy.jernigan@gmail.com>
2019-09-03 22:01:27 -0400

    EndPoint Register

--- api/usersRouter.js
+++ api/usersRouter.js
@@ -7,11 +7,29 @@ const usersModel = require('./usersModel.js');
 
 const router = express.Router();
 
-//#region - User Login and Register
+//#region Register
 
 // POST 	/api/register 	
   // Creates a user using the information sent inside the body of the request. 
   // Hash the password before saving the user to the database.
+
+server.post('/api/register', (req, res) => {
+  let user = req.body;
+
+  bcrypt.hashSync(user.password, 12);
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+//#endregion
+
+//#region 
 
 // POST 	/api/login 	
   // Use the credentials sent inside the body to authenticate the user. 
